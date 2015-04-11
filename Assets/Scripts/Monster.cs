55f30ed3bce3ab54f4f984ad90d3700f537bb44b
Simon Chauvin <contact@simonchauvin.com>
2015-04-11 12:49:12 +0200

    Basic monster AI and player controller.

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monster : MonoBehaviour
+{
+    private Player player;
+
+
+    private NavMeshAgent agent;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        agent.destination = player.transform.position;
+	}
+}
